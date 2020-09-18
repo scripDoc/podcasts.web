eedf6aaaad9ca8c42089d4bcdb1d83c9fe3e59cc
scripdoc <alesis.x@mail.ru>
2020-09-18 12:39:29 +0300

    Updates

---
+++ precache-manifest.86895d3f5cdce659927e36ad00e9c024.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efd4c88b8a28b303e1971dfccf8ab621",
+    "url": "/podcasts.web/index.html"
+  },
+  {
+    "revision": "98605d17392ce82cbe6e",
+    "url": "/podcasts.web/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "655c74d74496b512999c",
+    "url": "/podcasts.web/static/css/main.9537fcd3.chunk.css"
+  },
+  {
+    "revision": "98605d17392ce82cbe6e",
+    "url": "/podcasts.web/static/js/2.f68f1591.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/podcasts.web/static/js/2.f68f1591.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "655c74d74496b512999c",
+    "url": "/podcasts.web/static/js/main.b38bf46f.chunk.js"
+  },
+  {
+    "revision": "bc2e1e32abb698489fc0",
+    "url": "/podcasts.web/static/js/runtime-main.61734c73.js"
+  },
+  {
+    "revision": "26ec751ca25dd252746359ed1eaec053",
+    "url": "/podcasts.web/static/media/arrow.26ec751c.svg"
+  },
+  {
+    "revision": "af5e2372a639acd3799ad377a949809c",
+    "url": "/podcasts.web/static/media/microphone.af5e2372.svg"
+  }
+]);
